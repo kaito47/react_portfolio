8c5293a28b002d680546071b7a73aaa5514e26b3
Kait OShaughnessy <oshaughnessykm@gmail.com>
2020-07-19 23:44:52 -0400

    updating aboutMe page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import AboutMe from "./Components/pages/aboutMe";
 import Portfolio from "./Components/pages/portfolio";
-import NavTabs from "./Components/NavTabs";
+// import NavTabs from "./Components/NavTabs";
 
 
 function App() {
   return (
     <Router>
       <div>
-        <NavTabs />
         <Route exact path="/" component={AboutMe} />
         <Route exact path="/portfolio" component={Portfolio} />
       </div>
--- src/Components/pages/aboutMe.js
+++ src/Components/pages/aboutMe.js
@@ -1,14 +1,96 @@
-import React from "react";
 
-function About() {
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+import GitHubIcon from '@material-ui/icons/GitHub';
+// import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import Header from '../aboutMe/Header';
+import MainFeaturedPost from '../aboutMe/MainFeaturedPost';
+import FeaturedPost from '../aboutMe/FeaturedPost';
+import Sidebar from '../aboutMe/Sidebar';
+import Footer from '../aboutMe/Footer';
+
+
+const useStyles = makeStyles((theme) => ({
+    mainGrid: {
+        marginTop: theme.spacing(3),
+    },
+}));
+
+const sections = [
+    { title: 'Portfolio', url: '/portfolio' },
+];
+
+const mainFeaturedPost = {
+    title: 'Hi, my name is Kait',
+    description:
+        "",
+    image:
+        "https://www.nanalyze.com/app/uploads/2017/01/Brain-Computer-Interface-Teaser.jpg",
+    imgText: 'Brain-Computer Interface',
+    linkText: '',
+};
+
+const featuredPosts = [
+    {
+        title: 'A bit about me:',
+        description:
+            "I'm a junior front end developer, currently employed as a Clinicial Specialist in the Deep Brain Stimulation Division for Medtronic, Inc.",
+        image: '../public/profilepic.jpg',
+        imageText: 'Profile Pic of Kait',
+    },
+    {
+        title: 'Contact',
+        description:
+            'This is a wider card with supporting text below as a natural lead-in to additional content.',
+        image: 'https://www.nanalyze.com/app/uploads/2017/01/Brain-Computer-Interface-Teaser.jpg',
+        imageText: 'Image Text',
+        social: [
+            { name: 'GitHub', icon: GitHubIcon },
+            { name: 'Twitter', icon: TwitterIcon },
+        ],
+    },
+];
+
+// const posts = [post1, post2, post3];
+
+const sidebar = {
+    social: [
+        { name: 'GitHub', icon: GitHubIcon },
+        { name: 'Twitter', icon: TwitterIcon },
+    ],
+};
+
+export default function Blog() {
+    const classes = useStyles();
+
     return (
-        <div>
-            <h1>About Page</h1>
-            <p>
-                Hi I'm Kait. I can code some things. This is my portfolio!
-      </p>
-        </div>
+        <React.Fragment>
+            <CssBaseline />
+            <Container maxWidth="lg">
+                <Header title="Blog" sections={sections} />
+                <main>
+                    <MainFeaturedPost post={mainFeaturedPost} />
+                    <Grid container spacing={4}>
+                        {featuredPosts.map((post) => (
+                            <FeaturedPost key={post.title} post={post} social={sidebar.social} />
+                        ))}
+                    </Grid>
+                    {/* <Grid container spacing={5} className={classes.mainGrid}>
+                        <Main title="From the firehose" posts={posts} />
+                        <Sidebar
+                            title={sidebar.title}
+                            description={sidebar.description}
+                            archives={sidebar.archives}
+                            social={sidebar.social}
+                        />
+                    </Grid> */}
+                </main>
+            </Container>
+            <Footer title="Footer" description="Thanks for stopping by!" />
+        </React.Fragment>
     );
 }
-
-export default About;
